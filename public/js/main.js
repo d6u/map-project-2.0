37a1dc67ec084b7ec006afb664046066c62793f8
Daiwei Lu <daiweilu123@gmail.com>
2013-11-28 17:46:53 -0500

    Add tooltips

--- public/js/main.js
+++ public/js/main.js
@@ -84,9 +84,37 @@ angular.module('ngBackbone', [])
 });
 
 
+angular.module('ngBootstrap', [])
+// bs-tooltip, bs-tooltip-title, bs-tooltip-placement
+.directive('bsTooltip', function() {
+  return function(scope, element, attrs) {
+
+    var options = {
+      animation: false,
+      placement: attrs.bsTooltipPlacement,
+      title:     attrs.bsTooltipTitle,
+      container: 'body'
+    };
+
+    element.tooltip(options);
+    element.on('click', function() {
+      if (element.hasClass('search-result')) {
+        $('body').children('.tooltip').remove();
+      }
+    });
+    element.on('mousedown', function() {
+      if (element.hasClass('md-place-handle')) {
+        element.tooltip('destroy');
+        element.one('mouseup', function() { element.tooltip(options); });
+      }
+    });
+  };
+});
+
+
 // --- mapApp ---
 //
-var app = angular.module('mapApp', ['ngBackbone', 'ngAnimate']);
+var app = angular.module('mapApp', ['ngBackbone', 'ngAnimate', 'ngBootstrap']);
 
 
 // --- Controllers ---
