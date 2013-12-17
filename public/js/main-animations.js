479a96f25d9a65d8b44a5db3f1b04aaa19d296c0
Daiwei Lu <daiweilu123@gmail.com>
2013-12-17 18:19:00 -0500

    Remove place list enter animation due undetectable height

--- public/js/main-animations.js
+++ public/js/main-animations.js
@@ -1,10 +1,5 @@
 app.animation('.md-place-item', function() {
   return {
-    enter: function(element, done) {
-      var height = element.outerHeight();
-      element.css('height', 0);
-      element.animate({height: height}, 100, done);
-    },
     leave: function(element, done) {
       element.animate({height: 0}, 100, done);
     }
